f3c8de81930b9e240383a94ef26a9241b1e44f6c
nazim <nazimaitali88@gmail.com>
2020-10-06 18:31:01 +0100

    Updates

---
+++ precache-manifest.6d910bbf4f413ae828e01a7f3f099ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c9b71169302737a0f46d9575348bba",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "900293ef04fad5385b92",
+    "url": "/todo-list-react/static/css/main.65bc964e.chunk.css"
+  },
+  {
+    "revision": "3bc33f1c1d6e244bffb0",
+    "url": "/todo-list-react/static/js/2.5e2c3997.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/todo-list-react/static/js/2.5e2c3997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900293ef04fad5385b92",
+    "url": "/todo-list-react/static/js/main.9c5f89a1.chunk.js"
+  },
+  {
+    "revision": "ccbce8821a4587017821",
+    "url": "/todo-list-react/static/js/runtime-main.39c48962.js"
+  }
+]);
